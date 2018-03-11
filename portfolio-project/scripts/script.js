ef56bb562bc3b92dfffa4bafaf6c8114d97c9783
Vitalik Matviishyn <vmatviishyn@gmail.com>
2018-03-11 14:33:40 +0200

    Refactoring

--- portfolio-project/scripts/script.js
+++ portfolio-project/scripts/script.js
@@ -1,38 +1,31 @@
 //Scroll to
 $(document).ready(function(){
-    $('.go_to').click( function(){ // ловим клик по ссылке с классом go_to
-	var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
-        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
-	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 1000); // анимируем скроолинг к элементу scroll_el
+    $('.go_to').click( function(){
+	var scroll_el = $(this).attr('href'); 
+        if ($(scroll_el).length != 0) { 
+	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 1000); 
         }
-	    return false; // выключаем стандартное действие
+	    return false; 
     });
 });
 
-//Modal Window
-$(document).ready(function() { // вся мaгия пoсле зaгрузки стрaницы
-	$('a#go').click( function(event){ // лoвим клик пo ссылки с id="go"
-		event.preventDefault(); // выключaем стaндaртную рoль элементa
-		$('#overlay').fadeIn(400, // снaчaлa плaвнo пoкaзывaем темную пoдлoжку
-		 	function(){ // пoсле выпoлнения предъидущей aнимaции
-				$('#modal_form') 
-					.css('display', 'block') // убирaем у мoдaльнoгo oкнa display: none;
-					.animate({opacity: 1, top: '0%'}, 400); // плaвнo прибaвляем прoзрaчнoсть oднoвременнo сo съезжaнием вниз
-		});
-	});
+// Skillbar
+$('.skillbar').each(function() {
+	$(this).find('.skillbar-bar').animate({
+		width:$(this).attr('data-percent')
+	}, 2000);
+});
 
+//back to top
+var $btnTop = $(".btn-top")
+$(window).on("scroll", function() {
+	if ($(window).scrollTop() >= 20){
+		$btnTop.fadeIn();
+	} else {
+		$btnTop.fadeOut();
+	}
+});
 
-
-
-
-	/* Зaкрытие мoдaльнoгo oкнa, тут делaем тo же сaмoе нo в oбрaтнoм пoрядке */
-	$('#modal_close, #overlay').click( function(){ // лoвим клик пo крестику или пoдлoжке
-		$('#modal_form')
-			.animate({opacity: 0, top: '45%'}, 200,  // плaвнo меняем прoзрaчнoсть нa 0 и oднoвременнo двигaем oкнo вверх
-				function(){ // пoсле aнимaции
-					$(this).css('display', 'none'); // делaем ему display: none;
-					$('#overlay').fadeOut(400); // скрывaем пoдлoжку
-				}
-			);
-	});
+$btnTop.on("click", function() {
+	$("html, body").animate({scrollTop:0}, 900)
 });
