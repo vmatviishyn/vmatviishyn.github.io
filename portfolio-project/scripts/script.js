22bfec8f1e9c12267d139e86e925ef1ce1edb3e2
Vitalik Matviishyn <vmatviishyn@gmail.com>
2018-03-17 17:41:30 +0200

    Refactoring

--- portfolio-project/scripts/script.js
+++ portfolio-project/scripts/script.js
@@ -25,7 +25,42 @@ $(window).on("scroll", function() {
         $btnTop.fadeOut();
     }
 });
-
 $btnTop.on("click", function() {
     $("html, body").animate({ scrollTop: 0 }, 900)
+});
+
+$(function() {
+    $(".typed").typed({
+        strings: ["Developer", "Starting Front-end developer."],
+        // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+        stringsElement: null,
+        // typing speed
+        typeSpeed: 20,
+        // time before typing starts
+        startDelay: 1200,
+        // backspacing speed
+        backSpeed: 20,
+        // time before backspacing
+        backDelay: 500,
+        // loop
+        loop: true,
+        // false = infinite
+        loopCount: 5,
+        // show cursor
+        showCursor: false,
+        // character for cursor
+        cursorChar: "|",
+        // attribute to type (null == text)
+        attr: null,
+        // either html or text
+        contentType: 'html',
+        // call when done callback function
+        callback: function() {},
+        // starting callback function before each string
+        preStringTyped: function() {},
+        //callback for every typed string
+        onStringTyped: function() {},
+        // callback for reset
+        resetCallback: function() {}
+    });
 });
---
+++ portfolio-project/scripts/works.js
@@ -0,0 +1,31 @@
+var works = [{
+        smallImg: "images/works/work-1-small.png",
+        fullImg: "images/works/work-1-full.png",
+        title: "Test work 1",
+        text: "From PSD to HTML/CSS",
+        details: {
+            0: "HTML5",
+            1: "CSS3"
+        }
+    },
+    {
+        smallImg: "images/works/work-2-small.png",
+        fullImg: "images/works/work-2-full.png",
+        title: "Test work 2",
+        text: "From PSD to HTML/CSS",
+        details: {
+            0: "HTML5",
+            1: "CSS3"
+        }
+    },
+    {
+        smallImg: "images/works/work-3-small.png",
+        fullImg: "images/works/work-3-full.png",
+        title: "Test work 3",
+        text: "Bootstrap test work",
+        details: {
+            0: "HTML5",
+            1: "CSS3"
+        }
+    },
+]
