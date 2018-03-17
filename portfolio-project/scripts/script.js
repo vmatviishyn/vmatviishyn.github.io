f2a9d8cefbb78473dfd810078e94715717457615
Vitalik Matviishyn <vmatviishyn@gmail.com>
2018-03-17 12:27:47 +0200

    Navbar changed, myWorks added, gallery changed

--- portfolio-project/scripts/script.js
+++ portfolio-project/scripts/script.js
@@ -1,31 +1,36 @@
 //Scroll to
-$(document).ready(function(){
-    $('.go_to').click( function(){
-	var scroll_el = $(this).attr('href'); 
-        if ($(scroll_el).length != 0) { 
-	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 1000); 
+$(document).ready(function() {
+    $('.go_to').click(function() {
+        var scroll_el = $(this).attr('href');
+        if ($(scroll_el).length != 0) {
+            $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 1000);
         }
-	    return false; 
+        return false;
     });
 });
 
 // Skillbar
 $('.skillbar').each(function() {
-	$(this).find('.skillbar-bar').animate({
-		width:$(this).attr('data-percent')
-	}, 2000);
+    $(this).find('.skillbar-bar').animate({
+        width: $(this).attr('data-percent')
+    }, 2000);
 });
 
 //back to top
 var $btnTop = $(".btn-top")
 $(window).on("scroll", function() {
-	if ($(window).scrollTop() >= 20){
-		$btnTop.fadeIn();
-	} else {
-		$btnTop.fadeOut();
-	}
+    if ($(window).scrollTop() >= 20) {
+        $btnTop.fadeIn();
+    } else {
+        $btnTop.fadeOut();
+    }
 });
 
 $btnTop.on("click", function() {
-	$("html, body").animate({scrollTop:0}, 900)
+    $("html, body").animate({ scrollTop: 0 }, 900)
+});
+
+
+$(document).ready(function() {
+    $('a').nivoLightbox();
 });
